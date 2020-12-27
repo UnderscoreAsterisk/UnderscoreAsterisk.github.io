4592d769884ae041b8057043a701793a6ea851e1
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2020-12-27 17:39:50 +0530

    New website

---
+++ precache-manifest.63a010bc0cad0371d7bd5e1028a82a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab182f22a01a5f1c2a8300659b8ef45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bbb489168c37b1f9aaf",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbb489168c37b1f9aaf",
+    "url": "/static/js/main.6ac13fcf.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
